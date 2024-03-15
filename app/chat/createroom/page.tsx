c3f347cbccfb8cc077abec916bcbd348d2c0390f
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-03-15 14:56:48 +0530

    added room cards

--- app/chat/createroom/page.tsx
+++ app/chat/createroom/page.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+"use client";
+import React from "react";
 
 const page = () => {
-  return (
-    <div className='pt-24'>
-      Create Room
-    </div>
-  )
-}
+  const handleSubmit = () => {};
 
-export default page
+  return (
+    <div className="px-[37vw] py-[35vh]">
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form className="space-y-6" action="#">
+          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            Create a Room
+          </h5>
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your Name
+            </label>
+            <input
+              type="name"
+              name="name"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Enter Your Name"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="roomname"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Room Name
+            </label>
+            <input
+              type="roomname"
+              name="roomname"
+              id="roomname"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Enter Room Name"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            onSubmit={handleSubmit}
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Create Room
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
